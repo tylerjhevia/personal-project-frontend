f96900c5dec0981a473b0438f371a72ec186fa1d
Tyler Hevia <tylerhevia@gmail.com>
2017-09-11 12:29:14 -0600

    styled BookCard component

--- src/Components/BookCard.js
+++ src/Components/BookCard.js
@@ -1,14 +1,16 @@
 "use strict";
 exports.__esModule = true;
 var React = require("react");
+require("../Styles/BookCard.css");
 var BookCard = function (props) {
     var volumeInfo = props.book.volumeInfo;
     return (React.createElement("div", { className: "book-card" },
-        React.createElement("h4", null, volumeInfo.title),
-        React.createElement("h5", null, volumeInfo.authors ? volumeInfo.authors[0] : "No author, I guess"),
-        React.createElement("p", null, volumeInfo.description),
-        React.createElement("img", { src: volumeInfo.imageLinks
+        React.createElement("img", { className: "book-image", src: volumeInfo.imageLinks
                 ? volumeInfo.imageLinks.smallThumbnail
-                : "https://pbs.twimg.com/profile_images/565602752152076288/NxWEBoTo.jpeg" })));
+                : "https://pbs.twimg.com/profile_images/565602752152076288/NxWEBoTo.jpeg" }),
+        React.createElement("div", { className: "book-details" },
+            React.createElement("h4", { className: "book-title" }, volumeInfo.title),
+            React.createElement("h5", { className: "book-author" }, volumeInfo.authors ? volumeInfo.authors[0] : "No author, I guess"),
+            React.createElement("p", { className: "book-description" }, volumeInfo.description))));
 };
 exports["default"] = BookCard;
--- src/Components/SearchResults.js
+++ src/Components/SearchResults.js
@@ -7,7 +7,6 @@ var SearchResults = function (props) {
     var mappedResults = props.searchResults.map(function (book) {
         return React.createElement(BookCard_1["default"], { key: book.id, book: book });
     });
-    return (React.createElement("div", { className: "search-results-div" },
-        React.createElement("h3", null, mappedResults)));
+    return (React.createElement("div", { className: "search-results-div" }, mappedResults));
 };
 exports["default"] = SearchResults;
